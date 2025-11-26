262860c6cabd2abf73ed815fc0e52b15685a9436
Rose Hall <rose@hall.ly>
2025-11-26 14:13:14 -0500

    Schema Update

--- web/src/lib/db/schema.ts
+++ web/src/lib/db/schema.ts
@@ -1,16 +1,24 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { relations } from 'drizzle-orm';
+import { integer, numeric, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const games = sqliteTable('games', {
-	id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
+    id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
     name: text("id").notNull(),
     description: text("description").notNull(),
-    visibility: text("visibility").notNull(),
-    version: text("version").notNull()
+    visibility: text("visibility", { enum: ["public", "private", "personal"] }).notNull(),
+    version: text("version").notNull(),
+    github_author: text("github_author").notNull(),
+    github_repo: text("github_repo").notNull(),
+    owner_rc_Id: numeric("owner_rc_id").notNull(),
 });
 
 export const gameAuthors = sqliteTable('game_authors', {
     gameId: text("game_id").notNull().references(() => games.id),
     recurse_id: integer("recurse_id"),
     display_name: text("display_name"),
-    visibility: text("visibility").notNull(),
+    visibility: text("visibility", { enum: ["public", "private", "personal"] }).notNull(),
 }); // references games
+
+export const gamesRelations = relations(games, ({ many }) => ({
+    authors: many(gameAuthors),
+}));
