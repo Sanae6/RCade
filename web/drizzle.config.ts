d6052b66b74bd5a0f84f0d6fc46b7475dbe8f57d
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2025-11-26 19:01:08 -0500

    support db studio

--- web/drizzle.config.ts
+++ web/drizzle.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   schema: './src/lib/db/schema.ts',
   out: './drizzle/migrations',
   dialect: 'sqlite',
+  dbCredentials: {
+    url: ".wrangler/state/v3/d1/miniflare-D1DatabaseObject/42782fb72a7eb39466a9088f42d6227a99db4316333e4c6d8ec1dffe854018bb.sqlite",
+  },
 });
