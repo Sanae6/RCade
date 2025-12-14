d34a8cacf2927d4589801a4015c214dfeed92d4f
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2025-12-13 23:56:07 -0500

    404 on game not found

--- web/src/routes/games/[game_name]/[[game_version]]/+page.server.ts
+++ web/src/routes/games/[game_name]/[[game_version]]/+page.server.ts
@@ -1,10 +1,14 @@
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from '../$types';
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
     const response = await fetch('/api/v1/games/by-name/' + encodeURIComponent(params.game_name));
 
     if (!response.ok) {
-        throw new Error(`Failed to fetch game: ${response.status}`);
+        if (response.status === 404) {
+            error(404, 'Game not found');
+        }
+        error(response.status, `Failed to fetch game`);
     }
 
     const game = await response.json();
