ef25ab375421054a58ea40dbff5ecb75b2aa686d
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2025-12-14 01:36:05 -0500

    Revert "fix SharedArrayBuffer"
    This reverts commit ad7d8e807defd613248908c4f58be498b2589dd0.

--- engine/src/index.ts
+++ engine/src/index.ts
@@ -80,7 +80,7 @@ export class RCadeWebEngine {
         iframe.style.width = "336px";
         iframe.style.height = "262px";
         iframe.style.border = "none";
-        iframe.allow = "cross-origin-isolated; camera";
+        iframe.allow = "camera";
 
         let loaded = this.move(iframe, appUrl.toString());
 
