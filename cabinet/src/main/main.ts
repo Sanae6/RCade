002b96dda9d5cd5a34211ef1ed0c057e0a887056
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2025-12-16 03:03:36 -0500

    ignore exit if manifest is provided

--- cabinet/src/main/main.ts
+++ cabinet/src/main/main.ts
@@ -373,7 +373,7 @@ function createWindow(): void {
   mainWindow.webContents.on("before-input-event", (_event, input) => {
     if (input.type === "keyDown") {
       mainWindow.webContents.send("input-activity");
-      if (input.type === "keyDown" && input.code === "Escape" && !args.noExit) {
+      if (input.type === "keyDown" && input.code === "Escape" && !args.noExit && !args.manifest) {
         mainWindow.webContents.send("menu-key-pressed");
       }
     }
